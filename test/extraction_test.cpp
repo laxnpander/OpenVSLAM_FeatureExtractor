ee84da132890f5d1d83021cb93d2f8542fd002ec
alex <laxnpander@gmail.com>
2020-11-19 13:55:23 +0100

    Initial commit

---
+++ test/extraction_test.cpp
@@ -0,0 +1,29 @@
+
+
+// gtest
+#include <gtest/gtest.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/highgui.hpp>
+#include <orb_feature_extractor/orb_extractor.h>
+
+TEST(OrbFeatureExtractor, Basic)
+{
+  openvslam::feature::orb_extractor extractor(3000, 1.2, 8, 15, 3);
+
+  cv::Mat img = cv::imread("image.jpg");
+
+  cv::Mat descriptor;
+  std::vector<cv::KeyPoint> features;
+
+  extractor.extract(img, cv::Mat(), features, descriptor);
+
+  cv::Mat img_features;
+  cv::drawKeypoints(img, features, img_features);
+
+  cv::namedWindow("Features", CV_WINDOW_NORMAL);
+  cv::imshow("Features", img_features);
+  cv::waitKey(0);
+}
